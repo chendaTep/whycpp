2dc801946f2dfebb73537bfbe45afe587c662d3f
Tep Chenda <tepchenda932@gmail.com>
2026-02-21 13:10:30 +0700

    New folder of the new BigProjects

---
+++ BigProjects/school.cpp
@@ -0,0 +1,16 @@
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <thread>
+
+class Person {
+protected:
+  std ::string name;
+  int age;
+  int id;
+
+public:
+  Person(std ::string n, int a, int ID) : name(n), age(a), id(ID) {}
+
+  void displayInfo() { int winth std ::cout << }
+};
